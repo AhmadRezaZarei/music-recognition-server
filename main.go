466bc4c73399727636f3583ebe0205f4d121ef34
RayZarei <ahmadreza0zarei@gmail.com>
2023-04-20 11:03:48 +0300

    Add upload api

--- main.go
+++ main.go
@@ -3,12 +3,35 @@ package main
 import (
 	"fmt"
 	"github.com/AudDMusic/audd-go"
+	"github.com/gin-gonic/gin"
+	"log"
+	"net/http"
 	"os"
 )
 
 func main() {
 
 
+	router := gin.Default()
+
+	router.MaxMultipartMemory = 8 << 20 // 8 MiB
+
+	router.POST("/upload", func(c *gin.Context) {
+
+		file, _ := c.FormFile("file")
+		log.Println(file.Filename)
+
+		err := c.SaveUploadedFile(file, "./musics/mm.mp3")
+
+		if err != nil {
+			panic(err)
+		}
+
+		c.String(http.StatusOK, fmt.Sprintf("'%s' uploaded!", file.Filename))
+
+	})
+
+	router.Run(":8080")
 
 	client := audd.NewClient(os.Getenv("API_KEY"))
 	file, err := os.Open("./music.mp3")
@@ -20,6 +43,7 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
+
 	fmt.Printf("%s - %s.\nTimecode: %s, album: %s. ℗ %s, %s\n\n"+
 		"Listen: %s\nOr directly on:\n- Apple Music: %s, \n- Spotify: %s",
 		result.Artist, result.Title, result.Timecode, result.Album,
