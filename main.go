20bf16a02ad3d87f77022d36bcec1b8418daaf5b
RayZarei <ahmadreza0zarei@gmail.com>
2023-04-20 10:16:12 +0300

    Add .env file

--- main.go
+++ main.go
@@ -1,7 +1,28 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/AudDMusic/audd-go"
+	"os"
+)
 
 func main() {
-	fmt.Println("Hello world")
+
+
+
+	client := audd.NewClient(os.Getenv("API_KEY"))
+	file, err := os.Open("./music.mp3")
+
+	if err != nil {
+		panic(err)
+	}
+	result, err := client.Recognize(file, "apple_music,spotify", nil)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("%s - %s.\nTimecode: %s, album: %s. ℗ %s, %s\n\n"+
+		"Listen: %s\nOr directly on:\n- Apple Music: %s, \n- Spotify: %s",
+		result.Artist, result.Title, result.Timecode, result.Album,
+		result.Label, result.ReleaseDate, result.SongLink,
+		result.AppleMusic.URL, result.Spotify.ExternalUrls.Spotify)
 }
